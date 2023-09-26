fef96d92cdb0a4a586a291d00d845ac89ebdbbbe
IceOhlajdIce <iceohlajdice@mail.ru>
2023-09-26 14:25:48 +0300

    Lesson 11: finished

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -10,9 +10,17 @@ export const Main = () => (
     <Layout>
       <Tabs />
       <Routes>
+        <Route path="/" element={
+          <div className={style.home}>
+            <h2>Стартовая страница</h2>
+            <h3>Добро пожаловать!</h3>
+            <p>Выберите категорию</p>
+          </div>
+        } />
         <Route path='/category/:page' element={<List />}>
           <Route path='post/:id' element={<Modal />} />
         </Route>
+        <Route path="*" element={<div className={style.text}>404</div>} />
       </Routes>
     </Layout>
   </main>
