8c8238501b867ba5ce109b4b6324faeb8154da71
IceOhlajdIce <iceohlajdice@mail.ru>
2023-09-28 16:19:21 +0300

    Lesson 11: fixed

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,27 +4,19 @@ import Tabs from './Tabs';
 import List from './List';
 import {Modal} from '../Modal/Modal';
 import {Route, Routes} from 'react-router-dom';
+import {HomePage} from '../Pages/HomePage/HomePage';
 
 export const Main = () => (
   <main className={style.main}>
     <Layout>
       <Tabs />
       <Routes>
-        <Route path="/" element={
-          <div className={style.home}>
-            <h2>Стартовая страница</h2>
-            <h3>Добро пожаловать!</h3>
-            <p>Выберите категорию</p>
-          </div>
-        } />
+        <Route path="/" element={<HomePage />} />
         <Route path='/category/:page' element={<List />}>
           <Route path='post/:id' element={<Modal />} />
         </Route>
-        <Route path="*" element={<div className={style.text}>404</div>} />
-        <Route path="/auth"
-          element={<div className={style.auth}>
-            <h3>Выберите категорию</h3>
-          </div>} />
+        <Route path="*" element={<div className={style.auth}>404</div>} />
+        <Route path="/auth" element={<HomePage />} />
       </Routes>
     </Layout>
   </main>
---
+++ src/components/Pages/HomePage/HomePage.jsx
@@ -0,0 +1,9 @@
+import style from './HomePage.module.css';
+
+export const HomePage = () => (
+  <div className={style.home}>
+    <h2>Стартовая страница</h2>
+    <h3>Добро пожаловать!</h3>
+    <p>Выберите категорию</p>
+  </div>
+);
