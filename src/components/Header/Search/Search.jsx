d6649b6fa6ebe0b044f53e8662cbe48b298100ca
IceOhlajdIce <iceohlajdice@mail.ru>
2023-10-08 19:09:10 +0300

    Lesson 13: searchSaga

--- src/components/Header/Search/Search.jsx
+++ src/components/Header/Search/Search.jsx
@@ -1,11 +1,28 @@
 import style from './Search.module.css';
 import {SVG} from '../../../UI/SVG';
+import {useDispatch} from 'react-redux';
+import {useState} from 'react';
+import {searchRequest} from '../../../store/search/searchAction';
 
-export const Search = () => (
-  <form className={style.form}>
-    <input className={style.search} type='search' />
-    <button className={style.button}>
-      <SVG icon='SearchIcon' width='100%' height='100%'/>
-    </button>
-  </form>
-);
+export const Search = () => {
+  const dispatch = useDispatch();
+  const [search, setSearch] = useState('');
+  const handlerSubmit = (e) => {
+    e.preventDefault();
+    dispatch(searchRequest(search));
+  };
+
+  return (
+    <form className={style.form} onSubmit={handlerSubmit}>
+      <input
+        className={style.search}
+        type='search'
+        onChange={e => setSearch(e.target.value)}
+        value={search}
+      />
+      <button className={style.button}>
+        <SVG icon='SearchIcon' width='100%' height='100%'/>
+      </button>
+    </form>
+  );
+};
--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,12 @@ import {commentReducer} from './commentReducer';
 import {authReducer} from './auth/authReducer';
 import commentsReducer from './comments/commentsSlice';
 import postsReducer from './posts/postsSlice';
+import {searchReducer} from './search/searchReducer';
 import {configureStore} from '@reduxjs/toolkit';
+import createSagaMiddleware from 'redux-saga';
+import rootSaga from './saga';
+
+const sagaMiddleware = createSagaMiddleware();
 
 export const store = configureStore({
   reducer: {
@@ -12,7 +17,10 @@ export const store = configureStore({
     authReducer,
     postsReducer,
     commentsReducer,
+    searchReducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(tokenMiddleware),
+    getDefaultMiddleware().concat(tokenMiddleware, sagaMiddleware),
 });
+
+sagaMiddleware.run(rootSaga);
